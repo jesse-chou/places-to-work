80d54e5f118b119a77320aa9ebb3baf048b46cd0
Jesse Chou <jessechou7@gmail.com>
2023-12-14 22:39:26 -0500

    refactor: changed tailwind config to point to right paths

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 
+import Link from "next/link";
+
 export default function Signup() {
   return (
     <div>
       <section className="bg-gray-50 dark:bg-gray-900">
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-          <a
-            href="#"
+          <Link
+            href="/"
             className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
           >
             <img
@@ -15,7 +17,7 @@ export default function Signup() {
               alt="logo"
             />
             Flowbite
-          </a>
+          </Link>
           <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
